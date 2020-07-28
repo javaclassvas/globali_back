69c5aab8d09b7088c687b07f2963c7b1fa63305d
Vasily Alexeev <javaclassvas@gmail.com>
2020-07-29 02:15:26 +0300

    first test launch

--- src/firebase/firebase-client.ts
+++ src/firebase/firebase-client.ts
@@ -1,9 +1,14 @@
 import * as admin from 'firebase-admin';
+import * as serviceAccount from "./service-account-credentials.json"
 
-const serviceAccount = require('./service-account-credentials.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
+    projectId: serviceAccount.project_id,
+  }),
+
   databaseURL: "https://glob-medicals.firebaseio.com"
 });
 
